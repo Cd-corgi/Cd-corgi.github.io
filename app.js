99cf941848ddcabf965c9156ed4ec846ca290a16
Cd-corgi <systemjpc@hotmail.com>
2024-07-01 13:12:12 -0500

    Reforming to the last thing

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 let tenerTexto = document.getElementById("texto");
-let salidaTexto = document.getElementById("output");
-let errorL = document.getElementById("error");
+let salidaTexto = document.getElementById("resu");
+let errorL = document.getElementById("advertencia");
+let errorL1 = document.getElementById("ad1");
+let errorL2 = document.getElementById("ad2");
 let copiarBoton = document.getElementById("copy")
 const diccionario = { "a": "1", "e": "2", "i": "3", "o": "4", "u": "5", "A": "6", "E": "7", "I": "8", "O": "9", "U": "0", }
 
-tenerTexto.addEventListener("input", function () {
-    if (tenerTexto.value.length >= 1) {
-        tenerTexto.style.backgroundColor = "#686868"
-        errorL.setAttribute("hidden", true)
-        errorL.innerText = "El texto proporcionado debe tener al menos un carácter"
-        errorL.style.color = "#fff"
-    } else {
-        errorL.removeAttribute("hidden")
-        tenerTexto.style.backgroundColor = "#965151"
-    }
-})
+tenerTexto.value = ""
+salidaTexto.value = ""
+
+
+if (tenerTexto.value.length <= 1) {
+    errorL.removeAttribute("hidden")
+    errorL2.removeAttribute("hidden")
+    errorL1.innerHTML = "Ningún mensaje fue<br>encontrado"
+    salidaTexto.style.opacity = 0
+    copiarBoton.style.display = "none"
+}
 
 function invocar(numero) {
     if (tenerTexto.value.length < 1) {
         errorL.removeAttribute("hidden")
-        errorL.innerText = "El texto proporcionado debe tener al menos un carácter"
-        errorL.style.color = "#fff"
-        tenerTexto.style.backgroundColor = "#965151"
         return;
     } else {
-        tenerTexto.style.backgroundColor = "#686868"
         errorL.setAttribute("hidden", true)
+        errorL1.innerHTML = ""
+        errorL2.setAttribute("hidden", true)
+        copiarBoton.style.display = "flex"
         encriptado(tenerTexto.value, numero)
     }
 }
@@ -33,6 +34,8 @@ function invocar(numero) {
 function limpiarTexto() {
     tenerTexto.value = ""
     salidaTexto.value = ""
+    errorL.removeAttribute("hidden")
+    salidaTexto.style.opacity = 0
 }
 
 /**
@@ -41,6 +44,7 @@ function limpiarTexto() {
  * @param {number} funcion 0 = encriptar | 1 = desencriptar
  */
 function encriptado(texto, funcion) {
+    salidaTexto.style.opacity = 1
     var resultado = ""
     switch (funcion) {
         case 0:
@@ -55,6 +59,10 @@ function encriptado(texto, funcion) {
             salidaTexto.value = resultado.length < 1 ? "" : resultado
             break;
     }
+    if (window.innerWidth <= 991 && window.innerWidth >= 300) {
+        salidaTexto.style.height = '5px'
+        salidaTexto.style.height = salidaTexto.scrollHeight + 'px';
+    }
 }
 
 function copiar() {
@@ -62,10 +70,6 @@ function copiar() {
         salidaTexto.removeAttribute("disabled")
         salidaTexto.select()
         document.execCommand("copy")
-        copiarBoton.innerText="¡Copiado!"
-        setTimeout(() => {
-            copiarBoton.innerText="Copiar contenido"
-        }, 1500)
         salidaTexto.setAttribute("disabled", true)
     } catch (error) {
         alert(error)
